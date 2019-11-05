082f8c13a5e716a5832967ebe087528a15e76d05
Linnik <aleksandrlinnik1@gmail.com>
2019-11-05 23:00:25 +0300

    First commit

--- js/main.js
+++ js/main.js
@@ -3,4 +3,13 @@ $('.team-slide').slick({
     dots: true,
     slidesToShow: 3,
     slidesToScroll: 3,
+    adaptiveHeight: true,
+    responsive: [
+	    {
+	      breakpoint: 1200,
+	      settings: {
+	        slidesToShow: 2,
+	      }
+	    }
+    ]
   });
