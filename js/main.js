c9a5320b0fc3b5c0bee91c24b794b4f87a3eae79
aleksandrlinnik1 <aleksandrlinnik1@gmail.com>
2020-08-21 00:52:52 +0300

    ourServices update

--- js/main.js
+++ js/main.js
@@ -115,3 +115,44 @@ function mobileMenu () {
   bodY.classList.toggle('overflowYHidden');
 }
 
+function slider() {
+  if(window.innerWidth <= 1200) {
+    document.addEventListener('click', function(event) {
+      if (event.target.dataset.dotId) {
+        currentSlide(event.target.dataset.dotId)
+      };
+    });
+
+    let slideIndex = 1;
+    showSlides(slideIndex);
+
+
+    function currentSlide(n) {
+      showSlides(slideIndex = n);
+    }
+
+    function showSlides(n) {
+      let i;
+      const slides = document.getElementsByClassName("slider-item");
+      const dots = document.getElementsByClassName("slider-dots_item");
+      if (n > slides.length) {
+        slideIndex = 1
+      }
+      if (n < 1) {
+        slideIndex = slides.length
+      }
+      for (i = 0; i < slides.length; i++) {
+        slides[i].style.display = "none";
+      }
+      for (i = 0; i < dots.length; i++) {
+        dots[i].className = dots[i].className.replace(" active", "");
+      }
+      slides[slideIndex - 1].style.display = "block";
+      dots[slideIndex - 1].className += " active";
+    }
+  }
+}
+slider()
+window.addEventListener(`resize`, event => {
+  slider()
+}, false);
